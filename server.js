672fed2cbd6ae59c92e661224b99036fc6ee6a19
LAPTOP <madgranketombreider@gmail.com>
2026-01-29 13:04:22 +0900

    edit server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.locals.wss = wss;
 let reduceRemainderMs = 0;
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on ${PORT}`);
 });
 
