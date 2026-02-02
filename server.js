f0b3568cc0159519656271f8928921405c718a8f
LAPTOP <madgranketombreider@gmail.com>
2026-02-02 15:44:15 +0900

    add console.log

--- server.js
+++ server.js
@@ -969,7 +969,7 @@ function toDatetimeLocalString(utcString) {
                           type: 'modify',
                           amount: diff
                         });
-
+                        console.log('diff:', diff)
                         wss.clients.forEach(client => {
                           if (client.readyState === WebSocket.OPEN) {
                             client.send(message);
